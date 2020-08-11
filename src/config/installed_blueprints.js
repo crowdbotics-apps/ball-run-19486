ed4dc4656b278ae44133675dec59f06af4f58c4b
Crowdbotics <team@crowdbotics.com>
2020-08-11 01:11:20 +0000

    add package blueprint-react-native-maps-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Maps89537', human_name: 'Maps', access_route: 'Maps89537', icon: 'map'},
 { name: 'Tutorial89536', human_name: 'Tutorial', access_route: 'Tutorial89536', icon: 'question-circle'},
 { name: 'Messaging89535', human_name: 'Messaging', access_route: 'Messaging89535', icon: 'comments'},
 { name: 'Achievements89534', human_name: 'Achievements', access_route: 'Achievements89534'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Maps89537Navigator from '../features/Maps89537/navigator';
 import Tutorial89536Navigator from '../features/Tutorial89536/navigator';
 import Messaging89535Navigator from '../features/Messaging89535/navigator';
 import Achievements89534Navigator from '../features/Achievements89534/navigator';
@@ -16,6 +17,7 @@ import Achievements89534Navigator from '../features/Achievements89534/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Maps89537: { screen: Maps89537Navigator },
 Tutorial89536: { screen: Tutorial89536Navigator },
 Messaging89535: { screen: Messaging89535Navigator },
 Achievements89534: { screen: Achievements89534Navigator },
