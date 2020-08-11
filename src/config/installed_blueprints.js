47610cc1f7d9fee4ab65a509c485dd136d790ef4
Crowdbotics <team@crowdbotics.com>
2020-08-11 01:11:03 +0000

    add package blueprint-react-native-achievement-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Achievements89534', human_name: 'Achievements', access_route: 'Achievements89534'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Achievements89534Navigator from '../features/Achievements89534/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Achievements89534: { screen: Achievements89534Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
